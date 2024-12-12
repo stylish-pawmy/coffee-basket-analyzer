d6f19c24ff15dc6fe51feef5fff13868d90d6d05
mahdiben6262 <am@proda-connect.dz>
2024-12-12 17:11:23 +0100

    feat: add dummy post request

--- coffee_basket_analyzer/main.py
+++ coffee_basket_analyzer/main.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Body
 
 from dtos.product_dto import ProductDto
+from dtos.rule_dto import RuleDto
 from services.basket_service import BasketService
 
 app = FastAPI()
@@ -18,3 +20,17 @@ async def GetProductById(id: int):
 async def GetAllProducts():
     products: pd.DataFrame = BasketService.get_distinct_products(sales)
     return [ ProductDto(row) for _, row in products.iterrows()]
+
+
+@app.post("/rules")
+async def FindRules(
+    products: List[str] = Body(...),
+    support: float = Body(...),
+    confidence: float = Body(...),
+    lift: float = Body(...),
+    ):
+
+    return {
+        "rules" : [
+            RuleDto(consequent=products, antiscident=products, support=support, confidence=confidence, lift=lift)
+    ]}
